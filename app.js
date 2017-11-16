331f01cb7f1ee80bcfdc8eea6f5c362dd512934d
botanki <karzan@botani.nu>
2017-11-16 11:46:47 +0100

    Typo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express'),
 // '/var/www/html/.well-known' during
 // certification renewals
 // MAKE SURE the folder '/var/www/html' exists
-app.use(express.static('/var/www/html');
+app.use(express.static('/var/www/html'));
 
 app.listen(5000, function () {
   console.log('Webserver listening on port 5000');
